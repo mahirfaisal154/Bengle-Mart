cccc6c772d222e3eeab0764404d850bc54f0b87e
mahirfaisal154 <mahirfaisal154@gmail.com>
2026-06-24 23:45:48 +0600

    OFFER and NEWCOLLECTION sections have completed

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../component/Hero/Hero'
 import Popular from '../component/popular/Popular'
 import Offers from  '../component/offers/Offers'
+import NewCollections from '../component/NewCollections/NewCollections'
 const Shop = () => {
   return (
     <div>
       <Hero/>
       <Popular/>
       <Offers/>
+      <NewCollections/>
     </div>
   )
 }
