702f37cd691305c50f1663be87c0e511029ef27a
mahirfaisal154 <mahirfaisal154@gmail.com>
2026-06-25 11:49:31 +0600

    NewsLetter section has completed.

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -3,6 +3,7 @@ import Hero from '../component/Hero/Hero'
 import Popular from '../component/popular/Popular'
 import Offers from  '../component/offers/Offers'
 import NewCollections from '../component/NewCollections/NewCollections'
+import NewsLetter from '../component/NewsLetter/NewsLetter'
 const Shop = () => {
   return (
     <div>
@@ -10,6 +11,7 @@ const Shop = () => {
       <Popular/>
       <Offers/>
       <NewCollections/>
+      <NewsLetter/>
     </div>
   )
 }
